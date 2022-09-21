05ddab1c2eec82a61da4b0e91288b371b6611d48
dangomushi <dangomushi@Dangomushi.Dangomushi.net>
2022-09-21 17:18:29 +0900

    FIX: split memorymap function

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,18 @@ use uefi::prelude::*;
 use uefi::proto::media::file::{File, FileAttribute, FileMode, FileType};
 use uefi::table::boot::MemoryDescriptor;
 use uefi::table::runtime::ResetType;
+use alloc::vec::Vec;
+
+// Get memory map -> memorymap vec
+fn get_memory_map(boot_services: &BootServices) -> Vec<MemoryDescriptor> {
+    let mapSize = boot_services.memory_map_size().map_size;
+    let mut mapBuffer = vec![0; mapSize * 8];
+    info!("mapBuffer size: {}", mapSize);
+
+    let (mapKey, descItr) = boot_services.memory_map(&mut mapBuffer).unwrap();
+
+    descItr.copied().collect::<alloc::vec::Vec<_>>()
+}
 
 #[entry]
 fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
@@ -20,13 +32,8 @@ fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
 
     writeln!(&mut st.stdout(), "Hello, Musix!!\n").unwrap();
 
-    let mapSize = st.boot_services().memory_map_size().map_size;
-    let mut mapBuffer = vec![0; mapSize * 8];
-    info!("mapBuffer size: {}", mapSize);
+    let mut descriptors = get_memory_map(st.boot_services()); 
 
-    let (mapKey, descItr) = st.boot_services().memory_map(&mut mapBuffer).unwrap();
-
-    let descriptors = descItr.copied().collect::<alloc::vec::Vec<_>>();
     descriptors.iter().for_each(|descriptor| {
         info!(
             "{:?}, {}, {}, {}",
@@ -40,5 +47,6 @@ fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
         }
     }
 
-    Status::SUCCESS
+    // return exit status
+    // Status::SUCCESS
 }
