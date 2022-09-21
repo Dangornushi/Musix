3bddca4c0c25a3808ba89e2784a841655c3f4c21
nop <noplab90@gmail.com>
2022-09-21 18:09:18 +0900

    ビルド環境の再構築

---
+++ kernel/src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+#[no_mangle]
+pub extern "sysv64" fn kernel_main() {
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
