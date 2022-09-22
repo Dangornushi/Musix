207ebe387fc23bfb5438eb716eab72533348d5a1
Dangornushi <dangomushi@Yuma.local>
2022-09-22 23:31:53 +0900

    ADD: bootloaderからのピクセル描写

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -14,6 +14,9 @@ use core::mem;
 use goblin::elf::{program_header, Elf};
 use log::info;
 use uefi::prelude::*;
+use uefi::proto::console;
+use uefi::proto::console::gop;
+use uefi::proto::console::gop::{BltOp, BltPixel, FrameBuffer, GraphicsOutput, PixelFormat};
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::file::FileInfo;
 use uefi::proto::media::file::RegularFile;
@@ -27,6 +30,13 @@ use uefi_services::system_table;
 
 const KERNEL_BASE_ADDR: usize = 0x100000;
 const EFI_PAGE_SIZE: usize = 0x1000;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct FrameBufferInfo {
+    pub fb: *mut u8,
+    pub size: usize,
+}
 
 // load elf file (kernel file)
 fn load_kernel(boot_services: &BootServices, buf: Vec<u8>) -> usize {
@@ -120,24 +130,78 @@ fn get_memory_map(boot_services: &BootServices) -> Vec<MemoryDescriptor> {
     descItr.copied().collect::<alloc::vec::Vec<_>>()
 }
 
+#[allow(dead_code)]
+fn set_gop_mode(gop: &mut GraphicsOutput) {
+    let mut mode: Option<gop::Mode> = None;
+    for m in gop.modes().into_iter() {
+        //let m = m.unwrap();
+        let res = m.info().resolution();
+
+        // Hardcode for GPD Pocket / Lemur Pro.
+        if (mode.is_none() && (1024, 768) == res) || (1200, 1920) == res || (1920, 1080) == res {
+            mode = Some(m);
+        }
+    }
+
+    if let Some(mode) = mode {
+        gop.set_mode(&mode).unwrap();
+    }
+}
+
 #[entry]
 fn efi_main(image: Handle, mut st: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut st).unwrap();
 
     writeln!(&mut st.stdout(), "Hello, Musix!!\n").unwrap();
+    writeln!(&mut st.stdout(), "[ OK ] UEFI Prit\n").unwrap();
 
-    let mut descriptors = get_memory_map(st.boot_services());
+    let bt = st.boot_services();
+
+    let mut descriptors = get_memory_map(bt);
 
     // elf fileのエントリーポイント
-    let elf_entry = entry_kernel(st.boot_services(), image);
+    let elf_entry = entry_kernel(bt, image);
 
+    // GOP
+    let mut mode: Option<gop::Mode> = None;
+    let gop = if let Ok(gop) = unsafe { bt.locate_protocol::<GraphicsOutput>() } {
+        unsafe { &mut *gop.get() }
+    } else {
+        panic!("no ogp");
+    };
+
+    writeln!(&mut st.stdout(), "[ OK ] GOP\n").unwrap();
+
+    set_gop_mode(gop);
     // エントリーポイント先のアドレスの関数を作成
-    let Musix: extern "sysv64" fn() = unsafe { mem::transmute(elf_entry) };
+    let Musix: extern "sysv64" fn(fb: *mut FrameBuffer, mi: *mut gop::ModeInfo) =
+        unsafe { mem::transmute(elf_entry) };
+    writeln!(&mut st.stdout(), "[ OK ] make Musix function adress\n").unwrap();
 
+    const BYTES_PER_PIXEL: usize = 4;
+    let gop = unsafe { &mut (*gop) };
+    let mode = unsafe { gop.query_mode(0_u32) }.unwrap();
+    let mode_info = mode.info();
+    let (horizontal, vertical) = mode_info.resolution();
+    let stride = mode_info.stride();
+    let format = mode_info.pixel_format();
+    let mut fb = unsafe { gop.frame_buffer() };
+    for x in 0..horizontal {
+        for y in 0..vertical {
+            unsafe {
+                fb.write_value((stride * y + x) * BYTES_PER_PIXEL, [0_u8, 255_u8, 255_u8]);
+            }
+        }
+    }
+
+    let mut mi = gop.current_mode_info();
+    let mut fb = gop.frame_buffer();
     // カーネル実行(胸熱)
-    Musix();
-
-    // UEFIの全機能を停止
-    st.runtime_services()
-        .reset(ResetType::Shutdown, Status::SUCCESS, None)
+    Musix(&mut fb, &mut mi);
+    Status::SUCCESS
+    /*
+        // UEFIの全機能を停止
+        st.runtime_services()
+            .reset(ResetType::Shutdown, Status::SUCCESS, None)
+    */
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,13 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod graphics;
 use core::arch::asm;
+use graphics::{FrameBuffer, ModeInfo};
 
 // 2022/9/21
 // Musix Kernel
 
+#[derive(Copy, Clone, Debug)]
+pub struct PixelColor(pub u8, pub u8, pub u8); // RGB
+
 #[no_mangle]
-pub extern "sysv64" fn kernel_main() {
+pub extern "sysv64" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
     loop {
         unsafe {
             asm!("hlt");
