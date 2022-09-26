648bc374da8bd5efee9acf35bce9d14f93f94dfd
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 11:06:48 +0900

    DEL: background_render

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,14 +9,6 @@ use core::panic::PanicInfo;
 use kernel::console::Console;
 use kernel::graphics::{FrameBuffer, Graphics, ModeInfo, PixelColor};
 
-fn background_render(w: usize, h: usize, graphics: &mut Graphics) {
-    for y in 0..(h) {
-        for x in 0..(w) {
-            unsafe { graphics.write_pixel(x, y, PixelColor(10, 10, 10)) };
-        }
-    }
-}
-
 #[no_mangle]
 extern "C" fn kernel_main(fb: *mut FrameBuffer, mi: *mut ModeInfo) {
     let fb = unsafe { *fb };
